a67702b2bfed58f5f6409084f786ca115962d691
Lovis Rentsch <gpg@lovirent.eu>
2023-05-12 23:55:54 +0200

    multithread

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,65 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::thread;
 fn main() {
-    let hex = String::from("F4511E");
+    let hex = String::from("000000");
     // print(20, 20, hex);
     // println!("ok")
     let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    // loop {
+    //     for x in 0..1000 {
+    //         for y in 0..1000 {
+    //             print(x , y , &hex, &mut stream)
+    //         }
+    //     }
+    // }
+    thread::spawn(move || {
+        loop {
+            for x in 0..500 {
+                for y in 500..1000 {
+                    print(x , y , &hex, &mut stream)
+                }
+            }
+        }
+    });
+    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    let hex = String::from("333333");
+    thread::spawn(move || {
+        loop {
+            for x in 0..500 {
+                for y in 0..1000 {
+                    print(x , y , &hex, &mut stream)
+                }
+            }
+        }
+    });
+    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    let hex = String::from("ffffff");
+    thread::spawn(move || {
+        loop {
+            for x in 100..200 {
+                for y in 0..200 {
+                    print(x , y , &hex, &mut stream)
+                }
+            }
+        }
+    });
+    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    let hex = String::from("ffffff");
+    thread::spawn(move || {
+        loop {
+            for x in 0..100 {
+                for y in 0..200 {
+                    print(x , y , &hex, &mut stream)
+                }
+            }
+        }
+    });
+    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    let hex = String::from("000000");
     loop {
-        for x in 0..1000 {
-            for y in 0..1000 {
+        for x in (500..1000).rev() {
+            for y in (0..500).rev() {
                 print(x , y , &hex, &mut stream)
             }
         }
