129a02a629d359e7f7d9633dce7d90d482e7cee5
Lovis Rentsch <gpg@lovirent.eu>
2023-05-13 00:52:18 +0200

    rand position

--- src/main.rs
+++ src/main.rs
@@ -10,64 +10,38 @@ fn getclr() -> [u8; 4] {
         rand::thread_rng().gen_range(0..255),
     ]
 }
-fn main() {
-    // print(20, 20, hex);
-    // println!("ok")
-    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
-    thread::spawn(move || {
-        loop {
-            for x in 0..1000 {
-                for y in 0..200 {
-                    print(x , y , &getclr(), &mut stream)
-                }
-            }
-        }
-    });
-    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
-    thread::spawn(move || {
-        loop {
-            for x in 0..1000 {
-                for y in 200..400 {
-                    print(x , y , &getclr(), &mut stream)
-                }
-            }
-        }
-    });
-    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
-    thread::spawn(move || {
-        loop {
-            for x in 0..1000 {
-                for y in 400..600 {
-                    print(x , y , &getclr(), &mut stream)
-                }
-            }
-        }
-    });
-    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
-
-    thread::spawn(move || {
-        loop {
-            for x in 0..1000 {
-                for y in 600..800 {
-                    print(x , y , &getclr(), &mut stream)
-                }
-            }
-        }
-    });
-    let mut stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
-    loop {
-        for x in (0..1000).rev() {
-            for y in (0..500).rev() {
-                print(x , y , &getclr(), &mut stream)
-            }
-        }
-    }
+fn getcoorx() -> u32 {
+    rand::thread_rng().gen_range(0..1080)
 }
-fn print(x: i32, y: i32, hex: &[u8; 4], mut stream: &TcpStream) {
+fn getcoory() -> u32 {
+    rand::thread_rng().gen_range(0..1800)
+}
+fn main() {
+    let stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    thread::spawn(move || {
+        print(getcoorx(), getcoory(), &getclr(), &stream)
+    });   let stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    thread::spawn(move || {
+        print(getcoorx(), getcoory(), &getclr(), &stream)
+    });   let stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    thread::spawn(move || {
+        print(getcoorx(), getcoory(), &getclr(), &stream)
+    });   let stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    thread::spawn(move || {
+        print(getcoorx(), getcoory(), &getclr(), &stream)
+    });
+    let stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    thread::spawn(move || {
+        print(getcoorx(), getcoory(), &getclr(), &stream)
+    });
+    let stream = TcpStream::connect("192.168.120.13:1337").expect("Error connecting");
+    loop {
+        print(getcoorx(), getcoory(), &getclr(), &stream)
+    };
+}
+fn print(x: u32, y: u32, hex: &[u8; 4], mut stream: &TcpStream) {
     // let buf = format!("PX {x} {y} {hex}\n");
     let buf = format!("PX {x} {y} {:02x?}{:02x?}{:02x?}{:02x?}\n", hex[0], hex[1], hex[2], hex[3]);
-    // println!("{buf}");
-    // pintln!("{x} {y}");
     let buffer = buf.as_bytes();
     stream.write(&buffer).expect("Error writing");
 }
